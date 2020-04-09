c1abe0302b9cd8e5f52dc306229dc87c87fd5b02
jsmile <jsmile@naver.com>
2020-04-09 11:40:10 +0900

    init jpa-jta-tx

---
+++ src/main/java/com/jsmile/springboot/jpajtatx/JpaJtaTxApplication.java
@@ -0,0 +1,13 @@
+package com.jsmile.springboot.jpajtatx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaJtaTxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaJtaTxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jsmile/springboot/jpajtatx/JpaJtaTxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsmile.springboot.jpajtatx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaJtaTxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
